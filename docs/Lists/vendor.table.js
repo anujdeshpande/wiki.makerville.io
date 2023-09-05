8df798e85455704ead487ca65e25566439a7b242
Anuj Deshpande <anuj@makerville.io>
2023-09-05 11:01:52 +0530

    Add sortable table and react component

---
+++ docs/Lists/vendor.table.js
@@ -0,0 +1,262 @@
+import React from "react";
+
+// ----------------------------------------------------------------------------
+// Please respect chronological (date) order when adding new entries.
+// ----------------------------------------------------------------------------
+export const vendors = [
+    {
+	name: "Visha World",
+	type: "Components",
+	city: "Mumbai",
+	url: "http://www.vishaworld.com",
+	address: "357 Lamington Road,Ganesh Bhavan, 1st Floor,Opp. Lamington Road Police StationMumbai – 400007",
+    },
+    {
+	name: "MG Super Labs",
+	type: "Components",
+	city: "Nagpur",
+	url: "https://www.mgsuperlabs.co.in",
+	address: "-",
+    },
+    {
+	name: "Evelta",
+	type: "Components",
+	city: "Mumbai",
+	url: "https://evelta.com",
+	address: "Office No-1003, 10th Floor, Ellora Fiesta,B.R Ambedkar Marg, Jui Nagar,Navi Mumbai, India - 400705",
+    },
+    {
+	name: "Crazypi",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://crazypi.com/",
+	address: " No.36 & 37, SML Lavender Apartments, Flat No.102, 3rd Cross, Nrupathunga Nagar, 7th Phase, JP Nagar, Bengaluru - 560078 KARNATAKA, INDIA"	
+    },
+    {
+	name: "Sunrom",
+	type: "Components",
+	city: "Ahmedabad",
+	url: "https://www.sunrom.com",
+	address: "Z-401, 4th Floor, Dev Castle,Nr. Jaymala Bus Stop, Isanpur, Ahmedabad, Gujarat, 382443",
+    },
+    {
+	name: "Nex Robotics",
+	type: "Components",
+	city: "Mumbai",
+	url: "http://www.nex-robotics.com/",
+	address: "Unit No. 13,Building No. 2 (A3), Sector 1,Millennium Business Park,Mahape, Navi Mumbai 400 710",
+    },
+    {
+	name: "Robomart",
+	type: "Components",
+	city: "Noida",
+	url: "https://robomart.com",
+	address: "C-27, First Floor, Sector 12, Noida-201301 (UP) India",
+    },
+
+    {
+	name: "Fabtolab",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://www.fabtolab.com",
+	address: "525, 3rd Floor, 16th Cross, 2nd A Main, Sector 6, HSR layout,Bangalore - 560102",
+    },
+    {
+	name: "Robu",
+	type: "Components",
+	city: "Pune",
+	url: "https://robu.in",
+	address: "Dynamic Logistics Trade Park, Sumant Building, Bhosari Alandi Rd, Dighi, Pune",
+    },
+    {
+	name: "Sumeet",
+	type: "Components",
+	city: "Kolkata",
+	url: "https://sumeetinstruments.com/",
+	address: "29-B Rabindra Sarani,4TH floor, Suite NO.427 Kolkata 700073",
+    },
+    {
+	name: "Tanotis",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://www.tanotis.com/",
+	address: "1st Floor, Site No. 1, SFHS, Nandini Layout,Bangalore - 560096",
+    },
+    {
+	name: "Electron Components",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://www.electroncomponents.com/",
+	address: "19/5, II Cross,SRK Garden,Jayanagar,Bangalore - 560 041",
+    },
+    {
+	name: "Rhydolabs",
+	type: "Components",
+	city: "Kochi",
+	url: "https://rhydolabz.com/",
+	address: "Golden Plaza Building, Chitoor Road, Cochin, Kerala 682018",
+    },
+    {
+	name: "Thingbits",
+	type: "Components",
+	city: "?",
+	url: "https://www.thingbits.net/",
+	address: "?",
+    },
+    {
+	name: "Potential Labs",
+	type: "Components",
+	city: "Hyderabad",
+	url: "https://potentiallabs.com",
+	address: "Store # G-3, D.No: 1-109/20, Bikshpathi Nagar, Kondapur (Adj to RTA Kondapur, Gachibowli-Miyapur Road), Hyderabad - 500084",
+    },
+    {
+	name: "Robokits",
+	type: "Components",
+	city: "Gandhinagar",
+	url: "https://robokits.co.in/",
+	address: "B1+B2+B3/5 GIDC, Electronics Estate, Sector 25, Gandhinagar, Gujarat - 382044",
+    },
+    {
+	name: "Tomson Electronics",
+	type: "Components",
+	city: "Kochi",
+	url: "https://www.tomsonelectronics.com",
+	address: "H.O Taara Mansion, Manikkiri Cross Road,Pallimukku, Kochi, Kerala,India, Pin: 682 016",
+    },
+    {
+	name: "ProtoCentral",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://protocentral.com/",
+	address: "526, 2nd Floor, 7th Cross, 10th Main,Jeevanbima Nagar,Bengaluru – 560075",
+    },
+    {
+	name: "Techtonics",
+	type: "Components",
+	city: "Ahmedabad",
+	url: "https://www.techtonics.in",
+	address: "F-215, City Center, Near Idgah Circle,Idgah Road, Prem Darwaza,Ahmedabad, Gujarat 380016",
+    },
+    {
+	name: "Probots",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://probots.co.in/",
+	address: "#28,1st Floor, Malagala, Nagarabhaavi Ring road, Bengaluru 5600091",
+    },
+    {
+	name: "Quadstore",
+	type: "Components",
+	city: "Trichy",
+	url: "https://quadstore.in",
+	address: "Quad Store, B.64 Ramalinga Nagar, 2nd Main Road, Woraiyur, Trichy-620003, Tamil Nadu",
+    },
+    {
+	name: "RCBazaar",
+	type: "Components",
+	city: "Bengaluru",
+	url: "http://rcbazaar.com/",
+	address: "RcBazaar,1193, 26th main,Jayanagar 9th block,Bangalore 560069",
+    },
+    {
+	name: "Component Masters",
+	type: "Components",
+	city: "Mumbai",
+	url: "http://www.componentmasters.co.in/",
+	address: "1A, Gomant Dham, Chunam lane, Off. Lamington Road, Mumbai",
+    },
+    {
+	name: "Rare Components",
+	type: "Components",
+	city: "Jabalpur",
+	url: "https://rarecomponents.com/",
+	address: "582/3, Chandra Chaya Residency, Anand Colony, Baldeobagh, Jabalpur 482002",
+    },
+    {
+	name: "3DPrintronics",
+	type: "Components",
+	city: "Noida",
+	url: "https://www.3dprintronics.com/",
+	address: "J-11, Kasna Industrial Area, Surajpur Site V,  Greater Noida, UP - 201310",
+    },
+    {
+	name: "Campus Component",
+	type: "Components",
+	city: "Pune",
+	url: "https://www.campuscomponent.com/?",
+	address: "Ackruti Chambers, Office No. 308, 3rd Floor,Near Laxminarayan Theater,  Swargate,Pune 411037",
+    },
+    {
+	name: "Elementz",
+	type: "Components",
+	city: "Trivandrum",
+	url: "https://www.elementzonline.com/",
+	address: "Capital Heights,Trivandrum, Kerala",
+    },
+    {
+	name: "RS Components",
+	type: "Components",
+	city: "Noida",
+	url: "https://in.rsdelivers.com/?",
+	address: "1701/1, 7th Floor, Tower No -I, Express Trade Tower – II, Sector-132, Noida - 201301",
+    },
+    {
+	name: "Mouser",
+	type: "Components",
+	city: "Bengaluru",
+	url: "https://mouser.in",
+	address: "#9 Esquire Center,Second Floor, A Wing, MG Road, Bangalore 560001",
+    },
+    {
+	name: "Lion Circuits",
+	type: "PCB",
+	city: "?",
+	url: "https://www.lioncircuits.com/",
+	address: "?",
+    },
+ 
+    // {
+    // 	name: "",
+    // 	type: "",
+    // 	city: "",
+    // 	url: "",
+    // 	address: "",
+    // },
+];
+
+// ----------------------------------------------------------------------------
+// PesterDataTable column definition
+// ----------------------------------------------------------------------------
+export const columns = [
+    {
+	Header: "Name",
+	accessor: "name",
+	className: "pester-data-table left",
+	Cell: ({ cell: { value }, row: { original } }) => (
+		<a href={`${original.url}`} target="blank" rel="noreferrer noopener">
+		{value}
+	    </a>
+	),
+    },
+    {
+	Header: "Type",
+	accessor: "type",
+	className: "pester-data-table left",
+  },
+    {
+	Header: "City",
+	accessor: "city",
+	className: "pester-data-table",
+    },
+    {
+	Header: "Address",
+	accessor: "address",
+	className: "pester-data-table",
+    },
+    // {
+    // 	Header: "Email",
+    // 	accessor: "email",
+    // 	className: "pester-data-table"
+    // },
+];
