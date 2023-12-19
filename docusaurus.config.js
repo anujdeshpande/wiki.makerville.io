e322000096f43f084b12a45cc755131903bfe8ea
Anuj Deshpande <anuj@makerville.io>
2023-12-19 23:11:39 +0530

    Update to new docusaurus version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,11 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+//
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -147,8 +150,8 @@ const config = {
 	  copyright: `Makerville Wiki © 2023 is licensed under CC BY-NC-SA 4.0`
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
     }),
 };
